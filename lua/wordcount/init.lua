18472094f0f0603d927834138ea47910bd60bb5d
Jakob <jakob@sub.se>
2023-12-09 17:11:24 +0100

    ok

--- lua/wordcount/init.lua
+++ lua/wordcount/init.lua
@@ -1,26 +1,14 @@
 local M = {}
 
-function M.setup()
-  local default_config = {
-      wpm = 230
-  }
+M.word_count = function()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+    local text = table.concat(lines, " ")
+    local count = select(2, text:gsub("%S+", ""))
+    print("Word Count: " .. count)
+end
 
-  local user_config = vim.g.wordcount_options or default_config
-  local function word_count()
-      local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-      local text = table.concat(lines, " ")
-      local count = select(2, text:gsub("%S+", ""))
-      return count
-  end
-
-  local function read_time()
-    local time = tostring(math.ceil(word_count() / user_config.wpm))
-    return time
-  end
-
-  vim.api.nvim_create_user_command('WordCount', function()
-      print(word_count() .. " words / " .. user_config.wpm .. " WPM = " .. read_time() .. " minutes.")
-  end, {})
+M.setup = function()
+    vim.api.nvim_create_user_command('WordCount', M.word_count, {})
 end
 
 return M
